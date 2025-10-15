78bc652bf3ac0e1c600512da941faaefc476421c
Sushil Sharma <79084384+blAcCkbiRd@users.noreply.github.com>
2025-10-15 19:40:29 +0000

    Created frontend branch

--- hope_spring/src/App.js
+++ hope_spring/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React frontend
         </a>
         <p>HopeSpring New Project</p>
       </header>
