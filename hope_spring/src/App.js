e653667298999e172e3d86e0252e992e18fa88fe
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2025-10-21 23:36:10 +0000

    Frontend Portion for Authentication part

--- hope_spring/src/App.js
+++ hope_spring/src/App.js
@@ -1,25 +1,18 @@
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Auth/Login';
+import Register from "./components/Auth/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>HopeSpring New Project</p>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
