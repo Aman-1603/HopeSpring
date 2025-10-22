578ab9a7d52742c0b2c4974bed2e5d99927a9e44
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2025-10-22 00:25:56 +0000

    Dashboard || Programs page ....

--- hope_spring/src/App.js
+++ hope_spring/src/App.js
@@ -4,6 +4,8 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Auth/Login';
 import Register from "./components/Auth/Register";
+import Dashboard from "./components/Pages/Dashboard";
+import Programs from "./components/Pages/Programs";
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/programs" element={<Programs />} />
+
+
       </Routes>
     </BrowserRouter>
   );
